0e5fb59bfada2e136cdb6caa813c979be10b21a5
Ankita Sarkar <ankitasarkar.it@gmail.com>
2026-02-11 06:05:25 +0100

    updates generate_pseudo

--- hybrid_model/src/features/generate_pseudo_trajectories_emothaw.py
+++ hybrid_model/src/features/generate_pseudo_trajectories_emothaw.py
@@ -6,6 +6,8 @@ from torchvision import transforms
 
 from src.models.reverse_model import ReverseModel
 
+print("THIS IS THE FILE BEING EXECUTED")
+
 # ---------------------------
 # CONFIG
 # ---------------------------
@@ -14,6 +16,7 @@ OUT_ROOT = "data/EMOTHAW/pseudo_trajectories"
 MODEL_PATH = "checkpoint/reverse_model.pth"
 
 TASK = "cursive_writing"
+print("TASK VALUE:", TASK)
 
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 SEQ_LEN = 200
